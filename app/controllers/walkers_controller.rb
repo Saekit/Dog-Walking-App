6fbfcc93d2bb3b33e2fec4764cd3d7e8af50627f
La Fountain <github email address>
2018-12-05 16:27:34 -0500

    Add navbar complete walker controller

--- app/controllers/walkers_controller.rb
+++ app/controllers/walkers_controller.rb
@@ -1,2 +1,60 @@
 class WalkersController < ApplicationController
+
+before_action :find_walker, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @walkers = Walker.all
+  end
+
+  def new
+    @walker = Walker.new
+  end
+
+  def create
+    @walker = Walker.create(walker_params)
+
+    respond_to do |format|
+      if @walker.valid?
+        @walker.save
+        format.html {redirect_to walker, notice: "You have successfully created a walker"}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @walker.update(walker_params)
+
+    respond_to do |format|
+      if @walker.valid?
+        @walker.save
+        format.html {redirect_to walker, notice: "You have successfully updated your form!"}
+      else
+        format.html render :edit
+      end
+    end
+  end
+
+  def destroy
+    @walker.destroy
+  end
+
+  private
+
+  def find_walker
+    @walker = Walker.find(params[:id])
+  end
+
+  def walker_params
+    params.require(:walker).permit(:age, :experience, :own_dogs, :ratings, :user_id )
+  end
+
+
 end
