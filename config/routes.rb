2d20418e89f376ba480bab389998c7a46a445bbc
La Fountain <github email address>
2018-12-05 09:49:32 -0500

    Delete service controller, add walker and admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'pages#welcome'
+
+  get 'pages/welcome'
+  get 'pages/about'
+  get 'pages/contact'
+
   resources :owners
   resources :users
-  resources :services
+  resources :walkers
+  resources :admins
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/controllers/walkers_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe WalkersController, type: :controller do
+
+end
