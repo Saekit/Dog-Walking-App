f91f0be62a26fc1fc3d825106ab763465cda6463
github username <raquel.sae.randall@gmail.com>
2018-12-05 18:40:20 -0500

    got login and sessions to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
   has_one :walker
   has_many :admins
   validates_uniqueness_of :username
+  validates :name, :email, :username, :role, :password, presence: true
 
   enum role: { owner: "owner", walker: "walker",  admin: "admin" }
 
-  class User
-    def password=(password) #setter/writer method
-      self.password_digest = BCrypt::Password.create(password) #giving the password_digest the bcrypted password that was entered
-    end
-  end
+  has_secure_password
 
 end
