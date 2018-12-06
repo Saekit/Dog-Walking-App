64ada6266af1b3cef4e4d6a5bcce9301ffd726d4
La Fountain <github email address>
2018-12-05 21:06:43 -0500

    Add pictures to About page

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,5 @@ class Dog < ApplicationRecord
   belongs_to :owner
   has_many :appointments
   has_many :walkers, through: :appointments
+
 end
--- app/models/service_appointment.rb
+++ app/models/service_appointment.rb
@@ -1,4 +1,6 @@
 class ServiceAppointment < ApplicationRecord
 belongs_to :appointment
 belongs_to :service
+
+accepts_nested_attributes_for :appointment, :service
 end
