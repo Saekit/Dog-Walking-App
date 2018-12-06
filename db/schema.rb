d02742c54e0630ded2f6a5f7046952f7b34913b1
La Fountain <github email address>
2018-12-06 13:24:28 -0500

    Begin work on appointment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_221549) do
+ActiveRecord::Schema.define(version: 2018_12_06_154129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2018_12_05_221549) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "date"
+    t.string "time"
   end
 
   create_table "dogs", force: :cascade do |t|
